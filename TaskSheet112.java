efeee88e4b059de089862240587edbe2f2969cc5
Prince Peñaflor <princepenaflor61@gmail.com>
2026-04-06 22:13:46 +0800

    ArithmeticTask

---
+++ TaskSheet112.java
@@ -0,0 +1,23 @@
+public class TaskSheet112 {
+
+    public static void main (String[] args){
+        
+        int result = 1 + 2;
+        System.out.println(result);
+
+        result -= 1; 
+        System.out.println(result);
+
+        result *= 2; 
+        System.out.println(result);
+
+        result /= 2; 
+        System.out.println(result);
+      
+        result += 8; 
+        
+        result %= 7; 
+        System.out.println(result);
+
+    }
+}
